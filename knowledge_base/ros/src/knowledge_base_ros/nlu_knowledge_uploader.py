99f83b83b0095874d5a71aaffcbec7a854615c42
Carlos Azevedo <cguerraazevedo@tecnico.ulisboa.pt>
2017-11-17 17:53:31 +0000

    slot dictionary updated and some bugs fixed

--- knowledge_base/ros/src/knowledge_base_ros/nlu_knowledge_uploader.py
+++ knowledge_base/ros/src/knowledge_base_ros/nlu_knowledge_uploader.py
@@ -30,7 +30,7 @@ class nlu_knowledge_upload(object):
     def __init__(self):
         # rospy.init_node('nlu_knowledge_uploader', anonymous=False)
         #subscribe to mbot_nlu_node -> natural_language_processing
-        #rospy.Subscriber("natural_language_processing", String, self.nluCallback)
+        #rospy.Subscriber("/mbot_nlu_node/natural_language_processing", String, self.nluCallback)
 
         # self.intention_list = ('go', 'grasp', 'bring')
         # self.intention_dict = {}
@@ -46,16 +46,66 @@ class nlu_knowledge_upload(object):
         #     self.slot_dict[x] = self.z
         #     self.z = self.z+1
 
-
-        # self.intention_dict = {'go':'move_base' , 'grasp':'grasp', 'put':'place', 'find':'find_person', 'introduce':'introduce', 'guide':'guide', '':'answer_question', '':'ask_name'}
-        #self.intention_to_action = {'go':'move_base' , 'grasp':'grasp', 'put':'place', 'bring':'place', 'find':'find_person', 'introduce':'introduce', 'guide':'guide'}
         self.intention_to_action = {'go':'move_base' , 'grasp':'grasp', 'meet':'introduce' , 'take':'place' , 'guide':'guide' , 'find':'find_person' , 'tell':'tell' , 'answer':'answer_question' }
         self.action_to_predicate = {'move_base':'at_r' , 'grasp':'holding', 'place':'on', 'find_person':'found', 'introduce':'known_p', 'guide':'at_p', 'answer_question':'iluminated' , 'tell':'told'}
-        #? - nove_base, guide, place
-
-        self.slot_to_type = {'b':'location', 'kitchen':'location' , 'dinner_table':'location', 'entrance':'location', 'coke':'object', 'milk':'object', 'mbot':'robot', 'John':'person', 'Lucy':'person'}
-        # self.slot_list_object = ('coke', 'energy drink', 'dinner_table')
-        
+        self.slot_to_type = {'summer': 'person', 'madison': 'person', 'freddie': 'person', 'bananas': 'object', 'office': 'location',
+                            'lewis': 'person', 'laptop': 'object', 'charles': 'person', 'toby': 'person', 'thomas': 'person',
+                            'joshua': 'person', 'peaches': 'object', 'iced tea': 'object', 'logan': 'person', 'bedside': 'location',
+                            'harrison': 'person', 'bedroom': 'location', 'faith': 'person', 'milk': 'object', 'emily': 'person',
+                            'blake': 'person', 'blueberries': 'object', 'pens': 'object', 'ham': 'object', 'tablet': 'object',
+                            'matthew': 'person', 'peanut': 'object', 'noah': 'person', 'josh': 'person', 'sugar': 'object',
+                            'TV stand': 'location', 'center table': 'location', 'ethan': 'person', 'orange': 'object',
+                            'peanuts': 'object', 'manju': 'object', 'scarlett': 'person', 'theo': 'person', 'plate': 'object',
+                            'coffee': 'object', 'rose': 'person', 'blackberries': 'object', 'food': 'object', 'luke': 'person',
+                            'samuel': 'person', 'fridge': 'location', 'oranges': 'object', 'amy': 'person', 'barbara': 'person',
+                            'dryer': 'object', 'watermelon': 'object', 'jack': 'person', 'desk': 'location', 'jacob': 'person',
+                            'bar': 'location', 'chips': 'object', 'bread': 'object', 'towel': 'object', 'onion': 'object',
+                            'sofa': 'location', 'dinning table': 'location', 'cider': 'object', 'alice': 'person', 'candy': 'object',
+                            'eleanor': 'person', 'arthur': 'person', 'rosie': 'person', 'knife': 'object', 'james': 'person',
+                            'biscuits': 'object', 'glasses': 'object', 'louis': 'person', 'cookies': 'object', 'isabelle': 'person',
+                            'knifes': 'object', 'tea': 'object', 'almond': 'object', 'lucy': 'person', 'bowls': 'object',
+                            'daisy': 'person', 'sponge': 'object', 'burger': 'object', 'almonds': 'object', 'ryan': 'person',
+                            'erika': 'person', 'noodles': 'object', 'william': 'person', 'container': 'object',
+                            'chewing gums': 'object', 'closet': 'location', 'harvey': 'person', 'alex': 'person', 'emma': 'person',
+                            'pen': 'object', 'daniel': 'person', 'living table': 'location', 'yogurt': 'object', 'forks': 'object',
+                            'choth': 'object', 'mints': 'object', 'blackberry': 'object', 'ken': 'person', 'pepper': 'object',
+                            'living room': 'location', 'rum': 'object', 'harry': 'person', 'lotion': 'object',
+                            'toothbrush': 'object', 'cake': 'object', 'snack': 'object', 'cookie': 'object', 'sink': 'location',
+                            'grapes': 'object', 'brooke': 'person', 'comb': 'object', 'sophie': 'person',
+                            'chocolate tablet': 'object', 'kitchen': 'location', 'water': 'object', 'bedroom chair': 'location',
+                            'tommy': 'person', 'tray': 'object', 'biscuit': 'object', 'jackie': 'person', 'magazine': 'object',
+                            'bookcase': 'location', 'donuts': 'object', 'emilia': 'person', 'amber': 'person', 'cheese': 'object',
+                            'henry': 'person', 'apple': 'object', 'sideshelf': 'location', 'charlotte': 'person',
+                            'sienna': 'person', 'michael': 'person', 'magazines': 'object', 'chewing gum': 'object',
+                            'apples': 'object', 'table': 'location', 'kitchen chair': 'location', 'florence': 'person',
+                            'trays': 'object', 'katie': 'person', 'sarah': 'person', 'bathroom': 'location', 'oliver': 'person',
+                            'isaac': 'person', 'sake': 'object', 'toothpaste': 'object', 'aaron': 'person', 'beer': 'object',
+                            'edward': 'person', 'amelie': 'person', 'jane': 'person', 'john': 'person', 'blueberry': 'object',
+                            'containers': 'object', 'fork': 'object', 'pencil': 'object', 'paprika': 'object', 'door': 'location',
+                            'sprite': 'object', 'cereals bar': 'object', 'oscar': 'person', 'coke': 'object', 'jamie': 'person',
+                            'cabinet': 'location', 'glass': 'object', 'strawberries': 'object', 'lemons': 'object',
+                            'amelia': 'person', 'grace': 'person', 'whisky': 'object', 'hot chocolate': 'object',
+                            'nathan': 'person', 'drawer': 'location', 'hanna': 'person', 'shampoo': 'object', 'bed': 'location',
+                            'banana': 'object', 'will': 'person', 'strawberry': 'object', 'books': 'object', 'lemonade': 'object',
+                            'can': 'object', 'charlie': 'person', 'newspaper': 'object', 'crackers': 'object',
+                            'newspapers': 'object', 'erik': 'person', 'toilet': 'location', 'vodka': 'object', 'chloe': 'person',
+                            'snacks': 'object', 'candies': 'object', 'sushi': 'object', 'senbei': 'object', 'pie': 'object',
+                            'juice': 'object', 'cans': 'object', 'onions': 'object', 'ivy': 'person', 'cream': 'object',
+                            'peter': 'person', 'lemon': 'object', 'zoe': 'person', 'peach': 'object', 'bowl': 'object',
+                            'couch': 'location', 'brian': 'person', 'book': 'object', 'corridor': 'location', 'plates': 'object',
+                            'martha': 'person', 'lily': 'person', 'soap': 'object', 'pringles': 'object', 'pizza': 'object',
+                            'cupboard': 'location', 'chocolate bar': 'object', 'samantha': 'person', 'ella': 'person',
+                            'drink': 'object', 'pear': 'object', 'dylan': 'person', 'chocolate egg': 'object', 'tyler': 'person',
+                            'notebook': 'object', 'evan': 'person', 'toilet paper': 'object', 'connor': 'person',
+                            'bottles': 'object', 'red bull': 'object', 'max': 'person', 'joseph': 'person', 'skyler': 'person',
+                            'cloth': 'object', 'charger': 'object', 'toiletries': 'object', 'counter': 'location', 'seth': 'person',
+                            'adam': 'person', 'bottle': 'object', 'olivia': 'person', 'elliot': 'person', 'paige': 'person',
+                            'salt': 'object', 'wine': 'object', 'nightstand':'location', 'hallway':'location', 'wardrobe':'location',
+                            'bookshelf':'location', 'coffee table':'location', 'sidetable':'location', 'dinning room':'location',
+                            'dinning room':'location', 'kitchen table':'location', 'kitchen cabinet':'location', 'rice':'object',
+                            'kleenex':'object', 'whiteboard cleaner':'object', 'cup':'object', 'drink':'object', 'food':'object',
+                            'cleaning stuff':'object', 'mia':'person', 'liam':'person', 'sophia':'person', 'abigail':'person',
+                            'mason':'person', 'alexander':'person', 'isabella':'person', 'ava':'person'}
 
 
     def map_nlu_to_predicates(self, reconized_intention, reconized_slot):
@@ -76,52 +126,20 @@ class nlu_knowledge_upload(object):
 
     # def nluCallback(self, msg):
     #     '''
-    #     This fuction will get executed each time a msg is published in natural_language_processing topic
+    #     This fuction will get executed upon receiving a msg on natural_language_processing topic
     #     '''
+    #     # parse the message in phrases
+    #     # map the intenttions and slots
+    #     # upload to the knowledgebase
 
-    #     '''
-    #     move to the kitchen counter, grasp the Energy drink and bring it to the dinner table
+    #     sentence_recognized = msg.data
 
-    #     nlu current output
-    #         recognized_action: go
-    #         slot: ['kitchen counter']
-    #         -
-    #         recognized_action: grasp
-    #         slot: ['Energy drink']
-    #         -
-    #         recognized_action: bring
-    #         slot: ['energy drink','dinner table']
-    #     '''
+    #     for phrase in sentence_recognized:
+    #     slot = []
+    #     for i in range(len(phrase[1])):
+    #         slot.append( phrase[1][i].split(' is')[0] )
 
-    #     # self.reconized_intention = msg.action
-    #     # self.reconized_slot = msg.arguments #list of arguments
-
-    #     #map  actions/arguments (from nlu) to predicates (ppdl domain)
-    #     attribute_name, value = self.map_nlu_to_predicates(msg.action, msg.arguments)
-        
-    #     print attribute_name
-    #     print value
-    #     #publish these in the knowledgebase
-
-    # def start_nlu_uploader(self):
-
-# def compute_key_values(args):
-#     '''
-#     splits names by using character -- to get the keys
-#     this is needed by rosplan KB to be able to upload facts
-#     example:
-#         l--locA ; key is "l"
-#         (at r--ghost l--locA) ; keys are "r" and "l"
-
-#     '''
-#     key_value_list = []
-#     for each in args[1:]:
-#         try:
-#             key_value_list.append(each.split('--'))
-#         except:
-#             rospy.logerr('Error ocurred, please make sure that you are correctly using key--object and that no negative preconditions are present')
-#             return
-#     return key_value_list
+    #     attribute_name, value = map_test.map_nlu_to_predicates(phrase[0], slot)
 
 
 def main():
@@ -131,16 +149,38 @@ def main():
     nlu_knowledge_uploader = upld.UploadPDDLKnowledge()
     print 'upld'
 
-    sentence_recognized = [ ['go' , ['mbot', 'b', 'madsa']] , ['grasp', ['coke']] , ['introduce', ['']]]
-
     map_test = nlu_knowledge_upload()   
 
+    #rospy.spin()
+
+    #DEBUG
+
+    # Example of nlu output msg
+    # recognized_action: take
+    # slot: ['coke is an object', 'kitchen is a destination']
+    # recognized_action: go
+    # slot: ['living room is a destination']
+    # recognized_action: find
+    # slot: ['person and is an object']
+    # recognized_action: tell
+    # slot: ['what time is what to tell']
+
+    # sentence_recognized = [['go', ['living room is a destination']],
+    #                        ['take', ['coke is an object', 'kitchen is a destination']], ['meet', ['']],
+    #                        ['find', ['person and is an object']], ['tell', ['what time is what to tell']]]
+
+    sentence_recognized = [['go', ['sidetable is a destination']],
+                           ['take', ['coke is an object', 'kitchen is a destination']], ['answer', ['']],
+                           ['find', ['alexander is a person', 'and is an object']] ]
 
     for phrase in sentence_recognized:
-        attribute_name, value = map_test.map_nlu_to_predicates(phrase[0], phrase[1])
+        slot = []
+        for i in range(len(phrase[1])):
+            slot.append( phrase[1][i].split(' is')[0] )
 
-        #DEBUG
-        nlu_knowledge_uploader.rosplan_update_knowledge(1, '', '', attribute_name, value, 0.0, 'ADD_KNOWLEDGE')
+        attribute_name, value = map_test.map_nlu_to_predicates(phrase[0], slot)
+
+        nlu_knowledge_uploader.rosplan_update_knowledge(1, '', '', attribute_name, value, 0.0, 'ADD_GOAL')
 
         # if (is a GOAL):
         #     nlu_knowledge_uploader.rosplan_update_knowledge(1, '', '', attribute_name, value, 0.0, 'ADD_GOAL')
@@ -149,10 +189,10 @@ def main():
 
     # print attribute_name
     # print value
-
-
-        
     print 'update kb'
+
+    #DEBUG
+    
 
 # rosservice call /kcl_rosplan/update_knowledge_base "update_type: 1
 # knowledge:
