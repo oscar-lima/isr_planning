9177b8a0f0e59ad9d17ae5bdb36ded28c6de07c8
Carlos Azevedo <cguerraazevedo@tecnico.ulisboa.pt>
2018-02-09 12:21:28 +0000

    added launch file and fixed minor bug in the default file path

--- planning_tools/intrinsic_knowledge_generator/ros/src/intrinsic_knowledge_generator_ros/intrinsic_knowledge_generator_node.py
+++ planning_tools/intrinsic_knowledge_generator/ros/src/intrinsic_knowledge_generator_ros/intrinsic_knowledge_generator_node.py
@@ -8,8 +8,7 @@ class generate_knowledge(object):
 		rospack = rospkg.RosPack()
 
 		# get the save file path from the parameter server
-		self.path = rospy.get_param("min_req_facts_path", rospack.get_path('planning_domains'))
-		self.path = self.path + '/common/monarch_robot/pddl/gpsr/problems'
+		self.path = rospy.get_param("min_req_facts_path", rospack.get_path('planning_domains') + '/common/monarch_robot/pddl/gpsr/problems')
 		# get file_name
 		self.file_name = rospy.get_param("min_req_facts_file_name", '/minimum_required_facts.pddl')
 		# get the dictionary from the gpsr_dict file under mbot_world_model
