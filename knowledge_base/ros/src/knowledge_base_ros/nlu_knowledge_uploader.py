07f704e49756cdf98966a42d12ebb594cf242b92
Carlos Azevedo <cguerraazevedo@tecnico.ulisboa.pt>
2018-02-16 17:42:52 +0000

    added place, find person, find object and follow

--- knowledge_base/ros/src/knowledge_base_ros/nlu_knowledge_uploader.py
+++ knowledge_base/ros/src/knowledge_base_ros/nlu_knowledge_uploader.py
@@ -10,6 +10,7 @@ import knowledge_base_ros.upload_knowledge as upld
 # import mapping from slot to the available types
 from mbot_world_model_ros.gpsr_dict import slots_dict
 
+from mbot_robot_class_ros import mbot as mbot_class
 
 class nlu_knowledge_upload(object):
     def __init__(self):
@@ -26,40 +27,43 @@ class nlu_knowledge_upload(object):
         self.loop_rate = rospy.Rate(rospy.get_param('~loop_rate', 10.0))
 
         # dictionary that translates between the nlu intention output and the corresponding actions on the pddl domain
-        self.intention_to_action = {'go':'move_base' , 'grasp':'grasp', 'meet':'introduce' , 'take':'place' , 'guide':'guide' , 'find':'find_person' , 'tell':'tell' , 'answer':'answer_question', 'follow':'follow' }
+        #self.intention_to_action = {'go':'move_base' , 'grasp':'grasp', 'meet':'introduce' , 'take':'place' , 'guide':'guide' , 'find':'find_person' , 'tell':'tell' , 'answer':'answer_question', 'follow':'follow' }
         
         # dictionary that translates between the actions on the pddl domain and the corresponding predicates on the pddl domain
-        self.action_to_predicate = {'move_base':'at_r' , 'grasp':'holding', 'place':'on', 'find_person':'found', 'introduce':'known_p', 'guide':'at_p', 'answer_question':'iluminated' , 'tell':'told', 'follow':'following'}
+        #self.action_to_predicate = {'move_base':'at_r' , 'grasp':'holding', 'place':'on', 'find_person':'found', 'introduce':'known_p', 'guide':'at_p', 'answer_question':'iluminated' , 'tell':'told', 'follow':'following'}
         
         # dictionary that translates between the nlu slot output and the corresponding types on the pddl domain
         self.slot_to_type = slots_dict
 
+        # Instantiating mbotRobot object
+        self.mbot = mbot_class.mbotRobot(disabled={'perception': True, 'people_following': True, 'yolo': True, 'misc': True, 'hri': False, 'manipulation': True, 'navigation': True})
+
 
     def map_arguments_to_type(self, reconized_arguments):
         # mapping the recognized arguments (output from the nlu) to the corresponding types of the pddl domain
-        self.arguments_types = []
+        arguments_types = []
         for arg in reconized_arguments:
             if (arg in self.slot_to_type):
-                self.arguments_types.append([self.slot_to_type[arg], arg])
+                arguments_types.append([self.slot_to_type[arg], arg])
             elif (arg == 'yourself' or arg == 'me'):
-                self.arguments_types.append([self.slot_to_type['person'], 'person'])
-            elif (arg == 'noperson'):
-                self.arguments_types.append([self.slot_to_type['person'], 'noperson'])
+                arguments_types.append([self.slot_to_type['person'], 'person'])
+            # elif (arg == 'noperson'):
+            #     arguments_types.append([self.slot_to_type['person'], 'noperson'])
 
-        return self.arguments_types
+        return arguments_types
 
     def split_slot(self, reconized_slot):
-        self.arguments = []
-        self.description = []
+        arguments = []
+        description = []
         for arg in reconized_slot:
-            self.arguments.append(arg.split(' is ')[0])
-            self.description.append(arg.split(' is ')[-1].split()[-1])
+            arguments.append(arg.split(' is ')[0])
+            description.append(arg.split(' is ')[-1].split()[-1])
 
         # concatenate with and underscore arguments or intentions that are separated by spaces  
-        for i in range(len(self.arguments)):
-            self.arguments[i] = self.arguments[i].replace(' ', '_')
+        for i in range(len(arguments)):
+            arguments[i] = arguments[i].replace(' ', '_')
 
-        return self.arguments, self.description
+        return arguments, description
 
     def map_nlu_to_pddl_domain(self, reconized_intention, reconized_slot):
 
@@ -72,6 +76,8 @@ class nlu_knowledge_upload(object):
         fact_intention = []
         fact_arguments = []
 
+#--------------------------------------MOVE-------------------------------------------------#
+#-------------------------------------------------------------------------------------------#
         if reconized_intention == 'go':
             for i in range(len(slot_description)):
                 if 'destination' == slot_description[i]:
@@ -80,7 +86,8 @@ class nlu_knowledge_upload(object):
 
             goal_arguments = self.map_arguments_to_type(goal_arguments)
 
-
+#---------------------------------------GRASP-----------------------------------------------#
+#-------------------------------------------------------------------------------------------#
         elif reconized_intention == 'grasp':
             for i in range(len(slot_description)):
                 if 'object' == slot_description[i]:
@@ -100,7 +107,8 @@ class nlu_knowledge_upload(object):
                 if (fact_arguments):
                     fact_arguments = self.map_arguments_to_type(fact_arguments)
 
-
+#---------------------------------------KNOW------------------------------------------------#
+#-------------------------------------------------------------------------------------------#
         elif reconized_intention == 'meet':
             for i in range(len(slot_description)):
                 if 'person' == slot_description[i]:
@@ -121,7 +129,23 @@ class nlu_knowledge_upload(object):
                 if (fact_arguments):
                     fact_arguments = self.map_arguments_to_type(fact_arguments)
 
+#---------------------------------------PLACE-----------------------------------------------#
+#-------------------------------------------------------------------------------------------#
+        elif reconized_intention == 'take':
+            for i in range(len(slot_description)):
+                if 'object' == slot_description[i]:
+                    goal_intention = 'on'
+                    goal_arguments.append(slot_arguments[i])
 
+            if (goal_arguments):
+                for i in range(len(slot_description)):
+                    if 'destination' == slot_description[i]:
+                        goal_arguments.append(slot_arguments[i])
+
+            goal_arguments = self.map_arguments_to_type(goal_arguments)
+
+#---------------------------------------GUIDE-----------------------------------------------#
+#-------------------------------------------------------------------------------------------#
         elif reconized_intention == 'guide':
             for i in range(len(slot_description)):
                 if 'person' == slot_description[i]:
@@ -145,13 +169,53 @@ class nlu_knowledge_upload(object):
                     fact_arguments = self.map_arguments_to_type(fact_arguments)
 
 
-        print 'upload goals to the knowledgebase'
-        print "Att: ", goal_intention
-        print "Val: ", goal_arguments
+#-------------------------------FIND PERSON OR OBJECT---------------------------------------#
+#-------------------------------------------------------------------------------------------#
+        elif reconized_intention == 'find':
+            for i in range(len(slot_description)):
+                if 'person' == slot_description[i]:
+                    goal_intention = 'found_p'
+                    person_name = slot_arguments[i]
+                    goal_arguments.append(person_name)
 
-        print 'upload facts to the knowledgebase'
-        print "Att: ", fact_intention
-        print "Val: ", fact_arguments
+                elif 'object' == slot_description[i]:
+                    goal_intention = 'found_obj'
+                    object_name = slot_arguments[i]
+                    goal_arguments.append(object_name)
+
+            if (goal_arguments):
+                for i in range(len(slot_description)):
+                    if 'source' == slot_description[i] or 'destination' == slot_description[i]:
+                        if (goal_intention == 'found_p'):
+                            fact_intention = 'at_p'
+                            fact_arguments.append(person_name)
+                            fact_arguments.append(slot_arguments[i])
+                        else:
+                            fact_intention = 'on'
+                            fact_arguments.append(object_name)
+                            fact_arguments.append(slot_arguments[i])
+
+                goal_arguments = self.map_arguments_to_type(goal_arguments)
+
+                if (fact_arguments):
+                    fact_arguments = self.map_arguments_to_type(fact_arguments)
+
+
+#---------------------------------------FOLLOW----------------------------------------------#
+#-------------------------------------------------------------------------------------------#
+        elif reconized_intention == 'follow':
+            for i in range(len(slot_description)):
+                if 'person' == slot_description[i]:
+                    goal_intention = 'following'
+                    goal_arguments.append(slot_arguments[i])
+
+            goal_arguments = self.map_arguments_to_type(goal_arguments)
+
+        print "GOAL_INTENTION: ", goal_intention
+        print "GOAL_ARGS: ", goal_arguments
+
+        print "FACT_INTENTION: ", fact_intention
+        print "FACT_ARGS: ", fact_arguments
         
         return goal_intention, goal_arguments, fact_intention, fact_arguments
 
@@ -210,22 +274,36 @@ class nlu_knowledge_upload(object):
                 print self.sentence_recognized
                 print '...........................'
 
+
                 # parse the message in phrases so each phrase can be translated into the pddl attributes and uploaded
-                for phrase in self.sentence_recognized:                    
-                    if (phrase.slot):
-                        # map the intentions and slots to predicates and types
-                        goal_intention, goal_arguments, fact_intention, fact_arguments = self.map_nlu_to_pddl_domain(phrase.recognized_action, phrase.slot)
+                if (self.sentence_recognized):
+                    for phrase in self.sentence_recognized:  
+                        successful_upload = False
 
-                        if(goal_arguments):
-                            # upload the translated goals to the knowledgebase
-                            self.nlu_knowledge_uploader.rosplan_update_knowledge(1, '', '', goal_intention, goal_arguments, function_value=0.0, update_type='ADD_GOAL')
+                        if (phrase.slot):
+                            # map the intentions and slots to predicates and types
+                            goal_intention, goal_arguments, fact_intention, fact_arguments = self.map_nlu_to_pddl_domain(phrase.recognized_action, phrase.slot)
 
-                            if(fact_arguments):
+                            if(goal_arguments):
+                                successful_upload = True
+
                                 # upload the translated goals to the knowledgebase
-                                self.nlu_knowledge_uploader.rosplan_update_knowledge(1, '', '', fact_intention, fact_arguments, function_value=0.0, update_type='ADD_KNOWLEDGE')
+                                self.nlu_knowledge_uploader.rosplan_update_knowledge(1, '', '', goal_intention, goal_arguments, function_value=0.0, update_type='ADD_GOAL')
 
-                        # the 'ADD_GOAL' flag sets these attributes to be uploaded as goals. Use the flag 'ADD_KNOWLEDGE' to upload as facts.
-                        # use 'REMOVE_KNOWLEDGE' and 'REMOVE_GOAL' to remove facts and goals, respectively.
+                                if(fact_arguments):
+                                    # upload the translated goals to the knowledgebase
+                                    self.nlu_knowledge_uploader.rosplan_update_knowledge(1, '', '', fact_intention, fact_arguments, function_value=0.0, update_type='ADD_KNOWLEDGE')
+
+                            # the 'ADD_GOAL' flag sets these attributes to be uploaded as goals. Use the flag 'ADD_KNOWLEDGE' to upload as facts.
+                            # use 'REMOVE_KNOWLEDGE' and 'REMOVE_GOAL' to remove facts and goals, respectively.
+
+                    if (not successful_upload):
+                        #Say sorry for not being able to upload a goal
+                        self.mbot.hri.say('I am sorry I cannot do it')
+
+                else:
+                    #Say sorry for not being able to upload a goal
+                    self.mbot.hri.say('I am sorry I cannot do it')
 
 
             # sleep the node for a predefined period of time to decrease the node resources load
